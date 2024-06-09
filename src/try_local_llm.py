ec3c84670ab545f030193634722945d356072ff3
N Froseth <nikita.froseth.development@gmail.com>
2024-06-09 09:39:37 -0500

    Added Chunking to PDFs and also using hi_res unstrucutred converter strategy

--- src/try_local_llm.py
+++ src/try_local_llm.py
@@ -6,18 +6,25 @@ from pathlib import Path
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from tqdm import tqdm
-from unstructured.partition.auto import partition
+
+# from unstructured.partition.auto import partition_pdf
+from unstructured.partition.pdf import partition_pdf
 
 from get_embedding import timing
 
 
 PDF_DIR = "pdf_note_output/unstructured_version"
 LLM_DIR = "pdf_note_output/llm_pass_version"
+# IMAGE_PATH = "pdf_note_output/unstructured_extracted_images"
+IMAGE_PATH = "/home/xoph/repos/github/nfroseth/world_graph/test_files/pdf_note_output/unstructured_extracted_images"
+
 
 def note_name(path, extension=".pdf"):
     return os.path.basename(path)[: -len(extension)]
+
 
 @timing
 def parse_all_pdf_dir(path):
@@ -31,23 +38,43 @@ def parse_all_pdf_dir(path):
         # name = note_name(path)
         files[path] = read_pdf(path)
 
-        write_out_path = str(path).replace(base_inpath.name, PDF_DIR).replace(".pdf", ".txt")
+        write_out_path = (
+            str(path).replace(base_inpath.name, PDF_DIR).replace(".pdf", ".txt")
+        )
         with open(write_out_path, mode="w", encoding="utf-8") as file:
             file.write(files[path])
 
     return files
 
-def get_and_write_llm(llm, template, files, path):
+@timing
+def read_pdf(path):
+    
+    elements = partition_pdf(
+        path,
+        strategy="hi_res",
+        infer_table_structure=True,
+        # extract_images_in_pdf=True,
+        # extract_image_block_output_dir=IMAGE_PATH,
+        # extract_image_block_types=["Image", "Table"],
+        # extract_image_block_to_payload=False,
+    )
+    return "\n\n".join([str(el) for el in elements])
+
+def get_and_write_llm(chain, files, path):
     base_inpath = Path(path)
 
     logging.info(f"Sending Notes to LLM...")
     llm_processed_files = {}
     for path, document in tqdm(files.items()):
         try:
-            write_out_path = str(path).replace(base_inpath.name, LLM_DIR).replace(".pdf", ".md")
-            llm_processed_files[write_out_path] = get_llm_response(llm, template, document).content
+            write_out_path = (
+                str(path).replace(base_inpath.name, LLM_DIR).replace(".pdf", ".md")
+            )
+            llm_processed_files[write_out_path] = get_llm_response(
+                chain, document
+            ).content
 
-            with open(write_out_path, mode="w+", encoding="utf-8") as file:
+            with open(write_out_path, mode="a+", encoding="utf-8") as file:
                 file.write(llm_processed_files[write_out_path])
         except Exception as e:
             logging.critical(f"Failed Local LLM Call on file: {path} with {e}")
@@ -56,27 +83,35 @@ def get_and_write_llm(llm, template, files, path):
 
 
 
-@timing
-def read_pdf(path):
-    elements = partition(path)
-    return "\n\n".join([str(el) for el in elements])
 
 @timing
-def get_llm_response(llm, template, context):
-    prompt = PromptTemplate(template=template, input_variables=["context"])
-    chain = prompt | llm
+def get_llm_response(chain, context):
     return chain.invoke(context)
+
+
+def chunk_text(splitter, files):
+    after_chunking = {}
+
+    for path, file in files.items():
+        file = files[path]
+        for idx, chunk in enumerate(splitter.split_text(file)):
+            chunk_path = str(path).replace(".pdf", f"_chunk_{idx}.pdf")
+            after_chunking[chunk_path] = chunk
+
+    return after_chunking
+
 
 def main():
     llm = ChatOpenAI(
-            openai_api_key="lm-studio",
-            openai_api_base="http://172.30.128.1:1234/v1",
-            model="SanctumAI/Meta-Llama-3-8B-Instruct-GGUF/meta-llama-3-8b-instruct.Q5_1.gguf",
-        )
+        openai_api_key="lm-studio",
+        openai_api_base="http://172.30.128.1:1234/v1",
+        model="SanctumAI/Meta-Llama-3-8B-Instruct-GGUF/meta-llama-3-8b-instruct.Q5_1.gguf",
+        temperature=0.8,
+    )
 
     template = """
-    System: You are a office assistant that uses unstructured text notes from pdf form convert the provided context into an Obsidian markdown document.
-    Make sure lists are separated and correctly formatted.
+    System: You are an expert in generating markdown documents. Format the following text in markdown with consistent headers, lists, and code blocks.
+    Make sure lists are separated and correctly formatted. You will be given chunked components of the document.
     Without changing any of the text only provide well formatted markdown output. Do not output anything else.
 
     ---
@@ -84,19 +119,30 @@ def main():
     Note: {context}
     
     """
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=4096,
+        chunk_overlap=512,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    prompt = PromptTemplate(template=template, input_variables=["context"])
+    chain = prompt | llm
 
     path = "/home/xoph/repos/github/nfroseth/world_graph/test_files/pdf_note_input"
 
     files = parse_all_pdf_dir(path)
 
     print(f"PDF files: {len(files)}")
+    chunks = chunk_text(text_splitter, files)
+    print(f"Chunks files: {len(chunks)}")
+
     # print(f"files: {files.keys()}")
     # print("---")
     # print(get_llm_response(llm, template, context).content)
-    after_llm = get_and_write_llm(llm, template, files, path)
-    print(f"LLM files: {len(after_llm )}")
-    print(f"LLM files: {after_llm.keys()}")
 
+    after_llm = get_and_write_llm(chain, chunks, path)
+    print(f"LLM files: {len(after_llm)}")
+    print(f"LLM files: {after_llm.keys()}")
 
 
 if __name__ == "__main__":
